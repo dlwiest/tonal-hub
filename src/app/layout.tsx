3b66f4076fa198c28029f377da0b236256717995
Derrick Wiest <me@dlwiest.com>
2024-06-19 12:10:53 -0400

    Applied Mantine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { MantineProvider, ColorSchemeScript } from "@mantine/core";
+
+import '@mantine/core/styles.css';
 import "./globals.css";
 import styles from "./layout.module.css";
 
@@ -17,10 +20,15 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <ColorSchemeScript />
+            </head>
             <body className={inter.className}>
-                <div className={styles.container}>
-                    {children}
-                </div>
+                <MantineProvider>
+                    <div className={styles.container}>
+                        {children}
+                    </div>
+                </MantineProvider>
             </body>
         </html>
     );
